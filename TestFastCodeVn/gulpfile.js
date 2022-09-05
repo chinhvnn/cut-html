ba8c979359688e2c6809b5baf0ed07937aaa7ec8
unknown <chinhqn09@gmail.com>
2022-09-05 09:40:23 +0700

    fastcode

---
+++ TestFastCodeVn/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  pug = require("gulp-pug");
+
+// run this task by typing in gulp pug in CLI
+gulp.task("pug", function () {
+  return gulp
+    .src("./app/views/temp/*.pug")
+    .pipe(watch("./app/views/temp/*.pug"))
+    .pipe(pug()) // pipe to pug plugin
+    .pipe(gulp.dest("./app/views")); // tell gulp our output folder
+});
