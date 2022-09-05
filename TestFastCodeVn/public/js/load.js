d4a3183c957f43b99c5c156b1b0158b9e908118c
unknown <chinhqn09@gmail.com>
2022-09-05 17:47:03 +0700

    FIG MIXINS

--- TestFastCodeVn/public/js/load.js
+++ TestFastCodeVn/public/js/load.js
@@ -1,3 +1,4 @@
 window.onload = function() {
-    document.getElementById("loader").classList.remove("loader")
+    document.getElementById("loader").classList.remove("loader");
+    document.getElementById("loader").removeChild(document.getElementById("loader").firstElementChild);
 }
